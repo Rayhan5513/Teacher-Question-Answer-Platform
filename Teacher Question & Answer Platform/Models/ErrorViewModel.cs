0269b93f9b9f4139465b944acffd3c1dbef83b76
Rayhan <mdrayhanali.2017@gmail.com>
2024-02-05 23:31:14 +0600

    Add project files.

---
+++ Teacher Question & Answer Platform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Teacher_Question___Answer_Platform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
