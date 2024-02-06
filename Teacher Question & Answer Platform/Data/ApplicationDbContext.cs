a6c01cd65b77ab10172a12d92578aa6c10f4acc7
Rayhan <mdrayhanali.2017@gmail.com>
2024-02-07 00:48:30 +0600

    Adds database creation code

---
+++ Teacher Question & Answer Platform/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Teacher_Question___Answer_Platform.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        //public DbSet<User> Users { get; set; }
+        //public DbSet<Train> Trains { get; set; }
+    }
+}
---
+++ Teacher Question & Answer Platform/Models/Role.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Teacher_Question___Answer_Platform.Models
+{
+    public class Role
+    {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RoleId { get; set; }
+
+       // [Required(ErrorMessage = "RollId is re")]
+        [EmailAddress]
+        public string? RoleName { get; set; }
+
+    }
+}
